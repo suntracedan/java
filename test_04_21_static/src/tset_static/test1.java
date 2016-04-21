ed2e5513471c009bc8dcd8b181afb68a755cd71d
suntracedan <13474153341@163.com>
2016-04-21 16:35:51 +0800

    remove

--- test_04_21_static/src/tset_static/test1.java
+++
@@ -1,9 +0,0 @@
-package tset_static;
-//同一个包的两个非私有类可以互相访问
-public class test1 {
-	private static int i=10;//类中被static修饰的对象，属于类本身，在类外只能用类名进行访问、但不能为private；
-	public void fun()//没有被static修饰的成员必须用类构建一个对象，通过构建的对象来访问成员
-	{
-		System.out.printf("%d\n",i);//在类外可以访问共有方法中包含的私有成员
-	}
-}
